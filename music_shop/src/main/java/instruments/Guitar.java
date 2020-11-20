b9bec98d07da2294bd49a077ed94f22465161f0c
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-11-20 12:43:45 +0000

    added polymorphism comment to Guitar

--- music_shop/src/main/java/instruments/Guitar.java
+++ music_shop/src/main/java/instruments/Guitar.java
@@ -1,6 +1,7 @@
 package instruments;
 
-public class Guitar extends Instrument {
+public class Guitar extends Instrument {     // Instrument parent class reference is used to refer to Guitar child class object.
+                                             // As the Guitar object has an "IS-A" relationship with Instrument it is considered polymorphic.
 
     private int numOfStrings;
 
